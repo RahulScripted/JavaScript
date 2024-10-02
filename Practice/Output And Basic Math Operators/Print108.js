9afa587e6cf5d24032c63329f014e83f3124e21b
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-03 02:42:58 +0530

    Target complete

---
+++ Practice/Output And Basic Math Operators/Print108.js
@@ -0,0 +1,6 @@
+// Print the number 108 using arithmetic operations on the numbers 9 and 12.
+
+
+
+
+console.log(12 * 9);
---
+++ Practice/Output And Basic Math Operators/PrintLearnCodingOnCodeChef.js
@@ -0,0 +1,5 @@
+// Print "Learn Coding on CodeChef" to the console.
+
+
+
+console.log("Learn Coding on CodeChef");
