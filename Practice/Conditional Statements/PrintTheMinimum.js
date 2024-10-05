bce6c6881b07653c3ae940db48125146fa11e90b
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-06 02:41:57 +0530

    Target complete

---
+++ Practice/Conditional Statements/PrintTheMinimum.js
@@ -0,0 +1,12 @@
+// Rectify the given code to print the smallest among them.
+
+
+
+
+let x = 2;
+let y = 3;
+let z = 1;
+
+if(x < y && x < z) console.log(x);
+else if(y < x && y < z) console.log(y);
+else console.log(z);
---
+++ Practice/Conditional Statements/RectifyTheCode.js
@@ -0,0 +1,12 @@
+// Rectify the given program output the name of student with the higher percentage. In case they score equal percentages, print 'equal'.
+
+
+
+
+let name_1 = "Ajay";
+let name_2 = "Sumit";
+
+let marks_1 = 98.4;
+let marks_2 = 98.4;
+
+if (marks_2 == marks_1) console.log("equal");
